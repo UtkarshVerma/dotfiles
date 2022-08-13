14b7ac0de2ea686d8f54ce26f3332e1ea45af97a
Utkarsh Verma <utkarshverma@protonmail.com>
2022-08-13 12:38:11 +0200

    nvim: Fix lsp require order

--- .config/nvim/lua/lsp/lspconfig.lua
+++ .config/nvim/lua/lsp/lspconfig.lua
@@ -1,3 +1,17 @@
+-- mason-lspconfig has to be set-up before nvim-lspconfig for automatic
+-- installation of LSP servers
+local status_ok, mason = pcall(require, "mason")
+if status_ok then
+    mason.setup()
+
+    local status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+    if status_ok then
+        mason_lspconfig.setup({
+            automatic_installation = true
+        })
+    end
+end
+
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
     return
@@ -20,6 +34,6 @@ lspconfig.sumneko_lua.setup({
         }
     }
 })
-
--- Formatting is handled by null-ls
-lspconfig.clangd.setup({})
+lspconfig.pyright.setup({})
+lspconfig.gopls.setup({})
+lspconfig.clangd.setup({}) -- formatting is handled by null-ls
