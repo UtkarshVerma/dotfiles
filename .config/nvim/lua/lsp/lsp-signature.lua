aee596450d6115289ec3823002be885226cbccff
Utkarsh Verma <utkarshverma@protonmail.com>
2022-08-29 12:49:54 +0200

    nvim: update lsp-signature config

--- .config/nvim/lua/lsp/lsp-signature.lua
+++ .config/nvim/lua/lsp/lsp-signature.lua
@@ -4,5 +4,14 @@ if not status_ok then
 end
 
 lsp_signature.setup({
-    hint_enable = false
+    floating_window = false,
+    handler_opts = {
+        border = "none"
+    },
+    doc_lines = 0,
+    padding = " ",
+
+    hint_enable = true,
+    hint_prefix = " ",
+    hint_scheme = "DiagnosticInfo"
 })
