ae5019fd05dbcbd4b0224b6877fd82d6c58e8100
Utkarsh Verma <utkarsh@bitbanged.com>
2024-03-19 07:51:46 +0530

    nvim: Configure mini-splitjoin\

--- .config/nvim/lua/plugins/mini-splitjoin.lua
+++ .config/nvim/lua/plugins/mini-splitjoin.lua
@@ -1,9 +1,29 @@
+---@alias plugins.mini.splitjoin.hook fun(positions: integer[]):integer[]
+
+---@class plugins.mini.splitjoin.config.hooks
+---@field hooks_pre? plugins.mini.splitjoin.hook[]
+---@field hooks_post? plugins.mini.splitjoin.hook[]
+
+---@class plugins.mini.splitjoin.config
+---@field split? plugins.mini.splitjoin.config.hooks
+---@field join? plugins.mini.splitjoin.config.hooks
 ---@type LazyPluginSpec[]
 return {
   {
     "echasnovski/mini.splitjoin",
     keys = { "gS" },
-    -- FIXME: Appends a trailing delimiter, which is annoying.
-    opts = {},
+    opts = function(_, _)
+      local splitjoin = require("mini.splitjoin")
+
+      ---@type plugins.mini.splitjoin.config
+      return {
+        join = {
+          hooks_post = {
+            splitjoin.gen_hook.del_trailing_separator(),
+            splitjoin.gen_hook.pad_brackets(),
+          },
+        },
+      }
+    end,
   },
 }
