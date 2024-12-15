0ea954704e4df6fc9fc5e8c6e6bc3e8009f94464
Utkarsh Verma <utkarsh@bitbanged.com>
2024-12-15 19:05:38 +0100

    nvim: minor change

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -6,8 +6,11 @@ return {
     dependencies = { "trouble.nvim" },
     cmd = { "TodoTrouble", "TodoTelescope" },
     keys = {
-      { "]t", "<cmd>lua require('todo-comments').jump_next()<cr>", desc = "Next todo comment" },
-      { "[t", "<cmd>lua require('todo-comments').jump_prev()<cr>", desc = "Previous todo comment" },
+      -- stylua: ignore start
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+      -- stylua: ignore end
+
       { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
       { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/fix/fixme (trouble)" },
       { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
