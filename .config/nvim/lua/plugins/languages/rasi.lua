2863370f0b717856436c8bf0c29f1d51711c459d
Utkarsh Verma <utkarsh@bitbanged.com>
2025-01-10 10:08:54 +0100

    nvim: add rasi support

---
+++ .config/nvim/lua/plugins/languages/rasi.lua
@@ -0,0 +1,12 @@
+---@type LazyPluginSpec[]
+return {
+  {
+    "nvim-treesitter",
+    ---@type plugins.treesitter.config
+    opts = {
+      ensure_installed = {
+        "rasi",
+      },
+    },
+  },
+}
