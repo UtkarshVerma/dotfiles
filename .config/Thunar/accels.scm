a4be8dcc6d43c3e85ba7b5aeb02bc1e29e3b87e0
Utkarsh Verma <utkarsh@bitbanged.com>
2025-01-12 10:37:53 +0100

    config: minor change

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -1,4 +1,4 @@
-; Thunar GtkAccelMap rc-file         -*- scheme -*-
+; thunar GtkAccelMap rc-file         -*- scheme -*-
 ; this file is an automated accelerator map dump
 ;
 ; (gtk_accel_path "<Actions>/ThunarBookmarks/cd269280e0af6706cb6370e8aa6e9129" "")
