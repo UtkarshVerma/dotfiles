e44a273dbc94b92a4410a07378d29d098e228289
Utkarsh Verma <utkarsh@bitbanged.com>
2025-03-22 23:18:22 +0100

    config: homedir cleanup

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -1,37 +1,49 @@
-# Store interactive Python shell history according to the XDG specification.
-# Set the `PYTHONSTARTUP` environment variable to this file.
-#
+#!/usr/bin/env python3
 # This entire thing is unnecessary post v3.13.0a3
-#   https://github.com/python/cpython/issues/73965
+# https://github.com/python/cpython/issues/73965
 
 
-def is_vanilla_python() -> bool:
+def is_vanilla() -> bool:
+    """:return: whether running "vanilla" Python <3.13"""
     import sys
 
-    return not hasattr(__builtins__, "__IPYTHON__") and "bpython" not in sys.argv[0]
+    return (
+        not hasattr(__builtins__, "__IPYTHON__")
+        and "bpython" not in sys.argv[0]
+        and sys.version_info < (3, 13)
+    )
 
 
 def setup_history():
-    import atexit
+    """read and write history from state file"""
     import os
+    import atexit
     import readline
     from pathlib import Path
 
-    # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html#variables
-    state_home = Path(os.environ.get("XDG_STATE_HOME") or "~/.local/state")
-    if not state_home.is_dir():
-        print("Error: XDG_STATE_HOME does not exist at", state_home)
+    # Check PYTHON_HISTORY for future-compatibility with Python 3.13
+    history = None
+    if env_path := os.environ.get("PYTHON_HISTORY"):
+        history = Path(env_path)
+    else:
+        # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html#variables
+        state_home = None
+        if state_home_env := os.environ.get("XDG_STATE_HOME"):
+            state_home = Path(state_home_env)
+        else:
+            state_home = Path.home() / ".local" / "state"
 
-    history_file = state_home / "python" / "history"
+        history = state_home / "python" / "history"
 
-    try:
-        readline.read_history_file(history_file)
-    except FileNotFoundError:
-        Path(history_file.parent).mkdir(parents=False, exist_ok=True)
-        open(history_file, "wb").close()
+    # https://github.com/python/cpython/issues/105694
+    if not history.is_file():
+        readline.write_history_file(
+            str(history)
+        )  # breaks on macos + python3  without this.
 
-    atexit.register(readline.write_history_file, history_file)
+    readline.read_history_file(history)
+    atexit.register(readline.write_history_file, history)
 
 
-if is_vanilla_python():
+if is_vanilla():
     setup_history()
--- root/etc/bash.bashrc
+++ root/etc/bash.bashrc
@@ -18,6 +18,9 @@ esac
 
 [ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
 
+# Do not clutter user home directory
+HISTFILE="$XDG_STATE_HOME/bash/history"
+
 # User config
 if [ -f "$XDG_CONFIG_HOME/bash/bashrc" ]; then
     . "$XDG_CONFIG_HOME/bash/bashrc"
