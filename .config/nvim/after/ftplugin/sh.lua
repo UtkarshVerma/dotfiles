6f76f8784c19a2f5383af006e5dd2b1bdf75debc
Utkarsh Verma <utkarsh@bitbanged.com>
2025-05-05 09:46:19 +0200

    nvim: disable diagnostics for .env

--- .config/nvim/after/ftplugin/sh.lua
+++ .config/nvim/after/ftplugin/sh.lua
@@ -2,3 +2,14 @@ local opt = vim.opt_local
 
 opt.colorcolumn = ""
 opt.formatoptions:remove({ "r", "c", "o" })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = ".env",
+  group = vim.api.nvim_create_augroup(".env", { clear = true }),
+  desc = "Disable diagnostics for .env files.",
+  callback = function(args)
+    vim.diagnostic.enable(false, {
+      bufnr = args.buf,
+    })
+  end,
+})
