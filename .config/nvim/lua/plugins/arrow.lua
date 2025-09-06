47999713a377e42ac0813b624aa745f5226ce9a9
Utkarsh Verma <utkarsh@bitbanged.com>
2025-09-06 13:43:15 +0200

    arrow: revert shift+h/l navigation

--- .config/nvim/lua/plugins/arrow.lua
+++ .config/nvim/lua/plugins/arrow.lua
@@ -16,28 +16,12 @@
 ---@field window? {border?: string}
 ---@field mappings? table<plugins.arrow.mapping_key,string>
 
----Navigate buffers in `dir`. In case of a trivial bookmark list, fall back to native buffer navigation.
----@param dir "next"|"previous"
-local function navigate(dir)
-  return function()
-    if vim.tbl_count(vim.g.arrow_filenames) <= 1 then
-      vim.cmd("b" .. dir)
-      return
-    end
-
-    local persist = require("arrow.persist")
-    persist[dir]()
-  end
-end
-
 ---@type LazyPluginSpec[]
 return {
   {
     "otavioschwanck/arrow.nvim",
     keys = {
       { "ga", desc = "Arrow" },
-      { "<s-h>", navigate("previous"), desc = "Previous buffer" },
-      { "<s-l>", navigate("next"), desc = "Next buffer" },
     },
     ---@type plugins.arrow.config
     opts = {
