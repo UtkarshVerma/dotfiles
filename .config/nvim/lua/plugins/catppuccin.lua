e254fd99959b9502fb120272eb68fc2eea600cc9
Utkarsh Verma <utkarsh@bitbanged.com>
2025-09-06 14:14:57 +0200

    nvim: use versioned releases

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -6,14 +6,9 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
-    version = false, -- NOTE: v1.9.0 lacks Snacks and blink.cmp.
     priority = 1000,
     ---@type plugins.catppuccin.config
-    opts = {
-      -- color_overrides = {
-      --   mocha = require("util").colors,
-      -- },
-    },
+    opts = {},
     config = function(_, opts)
       require("catppuccin").setup(opts)
       vim.cmd.colorscheme("catppuccin")
